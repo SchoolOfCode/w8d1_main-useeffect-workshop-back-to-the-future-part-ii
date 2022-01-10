78be8efe8d00f12001e0e99d7af8d3f0c0afccba
Nadja <nadjagrothe@gmail.com>
2022-01-10 15:28:10 +0000

    completed taks 1 → random number & id as props

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,21 +4,25 @@ import "./App.css";
 import PokemonViewer from "../PokemonViewer";
 
 function App() {
-  const [id, setId] = useState();
+   const [id, setId] = useState();
 
-  function handleClick() {
-    // TODO: Set id to be random number between 1 and 151
-    setId(151);
-  }
+   function handleClick() {
+      // TODO: Set id to be random number between 1 and 151
+      //create function that "picks" random number (math.random)
+      const randomID = Math.floor(Math.random() * 151);
+      console.log(randomID);
+      // return that number(id)
+      setId(randomID);
+   }
 
-  return (
-    <div className="App">
-      {/* TODO: call handleClick when button clicked */}
-      <button>Get Random Pokemon</button>
-      {/* TODO: hand down id as a prop */}
-      <PokemonViewer />
-    </div>
-  );
+   return (
+      <div className="App">
+         {/* TODO: call handleClick when button clicked */}
+         <button>Get Random Pokemon</button>
+         {/* TODO: hand down id as a prop */}
+         <PokemonViewer id={id} />
+      </div>
+   );
 }
 
 export default App;
