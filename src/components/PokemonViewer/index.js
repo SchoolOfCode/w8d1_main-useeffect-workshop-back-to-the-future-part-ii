7c14ec34f221e8ca8b15a42aecea181b963d07c6
sim89 <simrenkaursoor@gmail.com>
2022-01-10 17:39:19 +0000

    task 3 complete can see images

--- src/components/PokemonViewer/index.js
+++ src/components/PokemonViewer/index.js
@@ -4,7 +4,7 @@ function PokemonViewer({ id }) {
    // TODO: send http request to `https://pokeapi.co/api/v2/pokemon/${id}` and display the data!
    // HINT: you will need useState and useEffect!
 
-   const [pokemon, setPokemon] = useState("pokemon");
+   const [pokemon, setPokemon] = useState([]);
 
    //in useEffect function fetching data[done]
    //to fetch data need to make a const to fetch data and await data.[done]
@@ -12,13 +12,15 @@ function PokemonViewer({ id }) {
    // this will change pokemon state
    //useEffect runs when id is changed.
    useEffect(() => {
-      fetchPokemon(id);
-      async function fetchPokemon(id) {
-         const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-         const response = await data.json();
-         setPokemon(response);
+      async function fetchPokemon() {
+         const response = await fetch(
+            `https://pokeapi.co/api/v2/pokemon/${id}`
+         );
+         const data = await response.json();
+         setPokemon(data);
       }
-   }, [id]);
+      fetchPokemon();
+   }, [id, pokemon]);
 
    console.log(pokemon);
 
@@ -26,6 +28,7 @@ function PokemonViewer({ id }) {
       <div className="pokemon-viewer">
          <p>display pokemon with id {id} here!</p>
          <p className="pokemon">{pokemon.name}</p>
+         <img src={pokemon.sprites.front_default} alt={pokemon.name} />
       </div>
    ) : (
       <></>
